2840be02103300666d5f7c0d60441ad47ff55aaa
DarkHILL35 <eremeevivan2002@gmail.com>
2023-12-27 20:08:40 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq; // Для работы с JSON
+
+
+
+public class Wallet
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public decimal Balance { get; set; }
+}
+
+public static class WalletParser
+{
+    public static List<Wallet> Parse(string input)
+    {
+        List<Wallet> wallets = new List<Wallet>();
+
+        JObject jsonData = JObject.Parse(input);
+        decimal btcExchangeRate = GetExchangeRate("BTC"); // Получение курса BTC к USD
+
+        // В вашем JSON-ответе должны быть поля, содержащие адреса и балансы кошельков
+        // Предположим, что данные JSON имеют вид: {"address1": "123", "balance1": "1.23", "address2": "456", "balance2": "4.56", ...}
+
+        foreach (var property in jsonData.Properties())
+        {
+            if (property.Name.StartsWith("address"))
+            {
+                string address = property.Value.ToString();
+                string balancePropertyName = "balance" + property.Name.Replace("address", "");
+                string balanceString = jsonData[balancePropertyName]?.ToString();
+                if (balanceString != null &&
+                    Decimal.TryParse(balanceString, out decimal balance))
+                {
+                    Wallet wallet = new Wallet
+                    {
+                        Address = address,
+                        Balance = balance
+                    };
+                    wallet.Name = DetermineWalletNetwork(address); // Определение сети кошелька
+                    wallet.Balance *= btcExchangeRate; // Конвертация баланса в USD
+                    wallets.Add(wallet);
+                }
+            }
+        }
+
+        return wallets;
+    }
+
+    private static decimal GetExchangeRate(string currencyCode)
+    {
+        // Реализация получения курса валюты относительно USD
+        // Здесь должен быть ваш обработчик получения курса
+        return 1.0m; // В данном примере предполагаем, что курс валюты по отношению к USD равен 1
+    }
+
+    private static string DetermineWalletNetwork(string address)
+    {
+        // Реализация определения сети по адресу
+        // Здесь следует вставить логику для определения сети по адресу кошелька
+        return "Unknown"; // В этом примере предполагаем, что мы не можем определить сеть
+    }
+}
